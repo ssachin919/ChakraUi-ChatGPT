9112ba780aed73cba5c8936a9cc11884195810b0
Sachin Anand <30962353+ssachin919@users.noreply.github.com>
2024-06-23 08:04:15 +0530

    Create GeneratingComponentCode.jsx

---
+++ GeneratingComponentCode.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@chakra-ui/react";
+
+function MyButton() {
+  return (
+    <div>
+      <Button colorScheme="blue">Primary Button</Button>
+      <Button variant="outline" colorScheme="gray">Secondary Button</Button>
+    </div>
+  );
+}
+
+export default MyButton;
